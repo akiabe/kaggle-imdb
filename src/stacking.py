4147d43d2b9baeb1320746b16843a4b6f9fe802e
Akifumi Abe <akiabe0831@gmail.com>
2020-10-28 07:10:07 +0900

    update

--- src/stacking.py
+++ src/stacking.py
@@ -2,29 +2,9 @@ import glob
 import numpy as np
 import pandas as pd
 
+from sklearn.linear_model import LinearRegression
+#from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
-from functools import partial
-from scipy.optimize import fmin
-
-class OptimizeAUC:
-    def __init__(self):
-        self.coef = 0
-
-    def _auc(self, coef, X, y):
-        x_coef = X * coef
-        predictions = np.sum(x_coef, axis=1)
-        auc_score = metrics.roc_auc_score(y, predictions)
-        return -1.0 * auc_score
-
-    def fit(self, X, y):
-        partial_loss = partial(self._auc, X=X, y=y)
-        initial_coef = np.random.dirichlet(np.ones(X.shape[1]), size=1)
-        self.coef_ = fmin(partial_loss, initial_coef, disp=True)
-
-    def predict(self, X):
-        x_coef = X * self.coef_
-        predictions = np.sum(x_coef, axis=1)
-        return predictions
 
 def run(pred_df, fold):
     train_df = pred_df[pred_df.kfold != fold].reset_index(drop=True)
@@ -33,7 +13,11 @@ def run(pred_df, fold):
     xtrain = train_df[["lr_pred", "lr_cnt_pred", "lr_stem_pred"]].values
     xvalid = valid_df[["lr_pred", "lr_cnt_pred", "lr_stem_pred"]].values
 
-    opt = OptimizeAUC()
+    #scl = StandardScaler()
+    #xtrain = scl.fit_transform(xtrain)
+    #xvalid = scl.transform(xvalid)
+
+    opt = LinearRegression()
     opt.fit(xtrain, train_df.sentiment.values)
     preds = opt.predict(xvalid)
     auc = metrics.roc_auc_score(valid_df.sentiment.values, preds)
